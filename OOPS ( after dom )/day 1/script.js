94ec02cd865d3d2277f668baf6a1b8d259c7d9dc
Archit <archit.pandey0007@gmail.com>
2025-04-22 12:15:42 +0530

    OOPS WALA

---
+++ OOPS ( after dom )/day 1/script.js
@@ -0,0 +1,76 @@
+
+
+// esa object sirf data hold na kare unki functionality bhi hold kare
+
+
+
+// var obj ={
+
+//     name : "jhon",
+//     age : 25,
+//     greet : function(){console.log("helllo")}
+
+// }
+
+
+
+// constructor function ( old way )
+
+// function Ghar(){
+//  this.shehar = "delhi",
+//  this.kamra = 2,
+//  this.bathroom = 3,
+//  this.kitchen = 1
+// }
+
+// var ghar2 = new Ghar()
+// console.log(ghar2)
+
+
+
+
+
+// function Tofee(){
+//     this.flavour = "vanila",
+//     this.price = 1.99,
+//     this.expiry = "2024-05-12"
+
+// }
+// let t1 = new Tofee()
+
+
+
+
+// function Tofee(flavour,price,expiry){
+//     this.flavour = flavour;
+//     this.price = price;
+//     this.expiry = expiry;
+
+
+// }
+// let t1 = new Tofee("vanila",1.99,"2024-05-12")
+// let t2 = new Tofee("mango",1.2,"2024-08-12")
+// let t3 = new Tofee("litchi",1.5,"2024-03-12")
+
+
+
+
+
+
+
+
+
+
+// with class ( that we do now )
+
+class Toffee{
+constructor(flavour,price,expiry){
+    this.flavour = flavour;
+    this.price = price;
+    this.expiry = expiry;
+}}
+
+let T5 = new Toffee("mango",2,"2024");   // ham constructor ko chala rahe hai
+let T3 = new Toffee("guava",2,"2024"); 
+let T2 = new Toffee("litchii",2,"2024"); 
+
